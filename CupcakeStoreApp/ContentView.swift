562390149dd1abcf9a6e80aa59e6e388ab150b67
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-21 15:07:19 +0200

    Creating the Address view which we will navigate to using the navigation link.

--- CupcakeStoreApp/ContentView.swift
+++ CupcakeStoreApp/ContentView.swift
@@ -8,19 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // we are using @StateObject  because this is the only place where the order will be created in the app then
+    //The rest or other views will observe the object so they Will use the @ObservedObject.
+    @StateObject var order = Order()
+    
+    
     var body: some View {
         
-        // we are using @StateObject  because this is the only place where the order will be created in the app then
-        //The rest or other views will observe the object so they Will use the @ObservedObject.
-        @StateObject var order = Order()
-        
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            Form{
+                Section{
+                    Picker("Select your cake type", selection: $order.type){
+                        ForEach(Order.types.indices){
+                            Text(Order.types[$0])
+                        }
+                    }
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                Section{
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation())
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section{
+                    NavigationLink{
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery Details")
+                    }
+                }
+                
+            }
+            .navigationTitle("Cupcake Corner")
         }
-        .padding()
+       
     }
 }
 
--- Order.swift
+++ Order.swift
@@ -13,7 +13,16 @@ class Order: ObservableObject {
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     @Published var type = 0
     @Published var quantity = 3
-    @Published var specialRequestEnabled = false
+    @Published var specialRequestEnabled = false {
+        //will run when the value is changing.
+        //This ensures that when we disable extra specialRequestEnabled then the rest must also be disabled.
+        didSet{
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
     @Published var extraFrosting = false
     @Published var addSprinkles = false
     
