23904bd0f88007777de94856764e3dab8d234052
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-21 14:35:19 +0200

    Initial Commit

---
+++ CupcakeStoreApp/CupcakeStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeStoreAppApp.swift
+//  CupcakeStoreApp
+//
+//  Created by Buhle Radzilani on 2024/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
